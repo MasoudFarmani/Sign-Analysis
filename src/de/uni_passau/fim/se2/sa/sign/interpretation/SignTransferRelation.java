66291c33b4e82b7f1f62f7712908ac83ecbfb6e4
Masoud Farmani <farman01@ads.uni-passau.de>
2025-06-26 13:36:59 +0200

    Implement SignTransferRelation.evaluate(Operation, SignValue, SignValue) for binary operation Implement evaluateAdd(SignValue, SignValue) helper function

--- src/de/uni_passau/fim/se2/sa/sign/interpretation/SignTransferRelation.java
+++ src/de/uni_passau/fim/se2/sa/sign/interpretation/SignTransferRelation.java
@@ -39,7 +39,63 @@ public class SignTransferRelation implements TransferRelation {
     Preconditions.checkNotNull(pLHS);
     Preconditions.checkNotNull(pRHS);
 
-    // TODO Implement me
-    throw new UnsupportedOperationException("Implement me");
+    if(pLHS == SignValue.BOTTOM || pRHS == SignValue.BOTTOM)
+        return SignValue.BOTTOM;
+
+    return switch (pOperation){
+        case ADD -> evaluateAdd(pLHS, pRHS);
+        case SUB -> evaluateSub(pLHS, pRHS);
+        case MUL -> evaluateMul(pLHS, pRHS);
+        case DIV -> evaluateDiv(pLHS, pRHS);
+        //Adding default just to make it exhaustive!
+        default -> throw new IllegalArgumentException("Unsupported Operation");
+    };
   }
+
+    private SignValue evaluateDiv(SignValue left, SignValue right) {
+        return null;
+    }
+
+    private SignValue evaluateMul(SignValue left, SignValue right) {
+        return null;
+    }
+
+    private SignValue evaluateSub(SignValue left, SignValue right) {
+        return null;
+    }
+
+    private SignValue evaluateAdd(SignValue left, SignValue right) {
+        return switch (left){
+            case PLUS -> switch (right){
+                case PLUS, ZERO, ZERO_PLUS -> SignValue.PLUS;
+                case MINUS, ZERO_MINUS, PLUS_MINUS, TOP -> SignValue.TOP;
+                default -> SignValue.BOTTOM;
+
+            };
+            case MINUS -> switch (right){
+                case MINUS, ZERO, ZERO_MINUS -> SignValue.MINUS;
+                case PLUS, ZERO_PLUS, PLUS_MINUS, TOP -> SignValue.TOP;
+                default -> SignValue.BOTTOM;
+            };
+            case ZERO -> right;
+            case ZERO_PLUS -> switch (right){
+                case PLUS -> SignValue.PLUS;
+                case ZERO, ZERO_PLUS -> SignValue.ZERO_PLUS;
+                case MINUS, ZERO_MINUS, PLUS_MINUS, TOP -> SignValue.TOP;
+                default -> SignValue.BOTTOM;
+            };
+            case ZERO_MINUS -> switch (right){
+                case MINUS -> SignValue.MINUS;
+                case ZERO, ZERO_MINUS -> SignValue.ZERO_MINUS;
+                case PLUS, ZERO_PLUS, PLUS_MINUS, TOP -> SignValue.TOP;
+                default -> SignValue.BOTTOM;
+            };
+            case PLUS_MINUS -> switch (right){
+                case ZERO -> SignValue.PLUS_MINUS;
+                default -> SignValue.TOP;
+            };
+            case TOP -> SignValue.TOP;
+            default -> SignValue.BOTTOM;
+        };
+    }
 }
